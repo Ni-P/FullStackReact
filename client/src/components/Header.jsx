07bff816857396766bdb56c1646f5fa7a59a100d
Niko Pinnis <niko.pinnis@gmail.com>
2017-12-13 19:24:53 +0200

    Add header auth status cases and login rediret route handler

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login with Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
+
   render() {
+    // console.log(this.props);
     return (
       <nav>
         <div className="nav-wrapper">
           <a className="left brand-logo" href="">
             Emaily
           </a>
-          <ul className="right">
-            <li>
-              <a href="">Login with Google</a>
-            </li>
-          </ul>
+          <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
