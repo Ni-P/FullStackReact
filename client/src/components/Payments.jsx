13ca724ae97602b8477a995bf5d4968c5edfc8bd
Niko Kristian Pinnis <niko.pinnis@gmail.com>
2017-12-18 18:43:29 +0200

    Add payment action and server payment route handler.

--- client/src/components/Payments.jsx
+++ client/src/components/Payments.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import StripeCheckout from 'react-stripe-checkout';
+import { connect } from 'react-redux';
+
+import * as actions from '../actions';
 
 class Payments extends React.Component {
   render() {
@@ -11,7 +14,7 @@ class Payments extends React.Component {
         name="Emaily"
         description="5$ for 5 Credits"
         amount={500}
-        token={token => console.log(token)}
+        token={token => this.props.handleToken(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}>
         <button className="btn">Add Credits</button>
       </StripeCheckout>
@@ -19,4 +22,4 @@ class Payments extends React.Component {
   }
 }
 
-export default Payments;
+export default connect(null, actions)(Payments);
