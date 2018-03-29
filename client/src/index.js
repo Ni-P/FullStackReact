dc1faac27b02e8ed22f7947d8cc4d45d3bc99936
Niko Pinnis <niko.pinnis@gmail.com>
2018-03-29 13:36:46 +0300

    Add Mailer class and surveyTemplate to send emails from /surveys route.

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import "materialize-css/dist/css/materialize.min.css";
-import reduxThunk from "redux-thunk";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import 'materialize-css/dist/css/materialize.min.css';
+import reduxThunk from 'redux-thunk';
 
-import App from "./components/App";
-import reducers from "./reducers";
+import App from './components/App';
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
@@ -14,7 +14,7 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.querySelector("#root")
+  document.querySelector('#root')
 );
 
 console.log(process.env.REACT_APP_STRIPE_KEY);
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,22 +2,32 @@ const mongoose = require('mongoose');
 
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
     const { title, subject, body, recipients } = req.body;
+    console.log(req.body.recipients);
 
     const survey = new Survey({
       title,
       subject,
       body,
       recipients: recipients.split(',').map(email => {
-        email.trim();
+        return {
+          email: email.trim()
+          //responded: false
+        };
       }),
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    console.log(survey.recipients);
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
